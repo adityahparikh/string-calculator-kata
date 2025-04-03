7873b1a426bb0067ccce256c90bca8bf1ef9904e
Aditya Parikh <aditya95parikh@gmail.com>
2025-04-03 13:35:00 +0530

    handle new line

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,8 +2,10 @@ class StringCalculator
     def self.add(numbers)
         return 0 if numbers.empty?
 
-        delimiter = ","
-        num_array = numbers.split(Regexp.escape(delimiter)).map(&:to_i)
+        deliminator = ","
+
+        numbers = numbers.gsub("\n", deliminator)
+        num_array = numbers.split(deliminator).map(&:to_i)
 
         num_array.sum
     end
