9131fe921ddc3594a248096b84ac255656be5f92
Aditya Parikh <aditya95parikh@gmail.com>
2025-04-03 13:19:06 +0530

    added simple string scenario

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,4 +1,10 @@
 class StringCalculator
     def self.add(numbers)
+        return 0 if numbers.empty?
+
+        delimiter = ","
+        num_array = numbers.split(Regexp.escape(delimiter)).map(&:to_i)
+
+        num_array.sum
     end
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,2 +1,24 @@
 require 'rspec'
 require_relative 'string_calculator'
+
+describe StringCalculator do
+    describe '.add' do
+      it 'returns 0 for an empty string' do
+        expect(StringCalculator.add("")) .to eq(0)
+      end
+
+      it 'returns the number itself for a single number' do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+  
+      it 'returns the sum of two numbers' do
+        expect(StringCalculator.add("1,5")).to eq(6)
+      end
+  
+      it 'returns the sum of multiple numbers' do
+        expect(StringCalculator.add("1,2,3,4")).to eq(10)
+      end
+      
+    end
+  end
+  
