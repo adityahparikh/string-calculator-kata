94cd3fcf7475fee464f595b09406d23c126b9735
Aditya Parikh <aditya95parikh@gmail.com>
2025-04-03 14:58:28 +0530

    Support for dynamic dilimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,10 +2,15 @@ class StringCalculator
     def self.add(numbers)
         return 0 if numbers.empty?
 
-        deliminator = ","
+        delimiter = ","
+        if numbers.start_with?("//")
+            partitions = numbers.split("\n", 2) # this ensures numbers are split into 2 parts
+            delimiter = partitions[0][2..-1]  # Extract delimiter after '//'
+            numbers = partitions[1]
+        end
 
-        numbers = numbers.gsub("\n", deliminator)
-        num_array = numbers.split(deliminator).map(&:to_i)
+        numbers = numbers.gsub("\n", delimiter)
+        num_array = numbers.split(delimiter).map(&:to_i)
 
         num_array.sum
     end
